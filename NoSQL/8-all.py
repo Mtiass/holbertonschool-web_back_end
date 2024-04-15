a2eb11bc248607dffdb9c9572c5d402aa1a3ae4f
Mtiass <MDLSproff@gmail.com>
2024-04-15 08:12:41 -0700

    8-all.py

---
+++ NoSQL/8-all.py
@@ -0,0 +1,12 @@
+!#/usr/bin/python3
+"""Python function that lists all documents in a collection."""
+
+
+def list_all(mongo_collection):
+    """mongo_collection will be the pymongo collection object."""
+    docs = list(mongo_collection.find())
+    if != docs:
+        empty = []
+        return empty
+    else:
+        return docs

85c0644a9c62bbea00b2d75a7b6a61dcd3d48526
Mtiass <MDLSproff@gmail.com>
2024-04-16 15:01:13 -0700

    Task sytnax fix

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,12 +4,12 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10)-> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     asy. coroutine that waits for a random delay and eventually returns it
     """
     delay = random.uniform(0, max_delay)
-    
+
     await asyncio.sleep(delay)
 
     return delay

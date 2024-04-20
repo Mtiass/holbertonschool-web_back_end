02eb20599c3271fe859a6930fb5c73ec682f0588
Mtiass <135639390+Mtiass@users.noreply.github.com>
2024-04-20 14:42:29 -0300

    fixing 11-schools_by_topic.py

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
+#!/bin/usr/env python3
 """Function that returns the list of school having a specific topic."""
-import pymongo
 
 
 def schools_by_topic(mongo_collection, topic):
@@ -8,8 +7,5 @@ def schools_by_topic(mongo_collection, topic):
     mongo_collection will be the pymongo collection object
     topic (string) will be topic searched
     """
-    query = {"topic": topic}
-    schlist = []
-    for sch in mongo_collection.find(query):
-        schools.append(school)
-    return schlist
+    schlist = mongo_collection.find({"topics": topic}))
+    return list(schlist)

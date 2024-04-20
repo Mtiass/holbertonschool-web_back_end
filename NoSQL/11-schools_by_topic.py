3416f1152a2e063cb9468180c7dd123cf3e71608
Mtiass <135639390+Mtiass@users.noreply.github.com>
2024-04-20 13:40:54 -0300

    possible fix for task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -7,6 +7,8 @@ def schools_by_topic(mongo_collection, topic):
     mongo_collection will be the pymongo collection object
     topic (string) will be topic searched
     """
+    if not isinstance(topic, str):
+        topic = str(topic)
     query = {"topic": topic}
     listdocs = list(mongo_collection.find(query))
     return listdocs

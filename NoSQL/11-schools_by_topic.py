bafc8650737f604fa593b1fd1817cc6d2f495158
Mtiass <MDLSproff@gmail.com>
2024-04-20 10:54:58 -0700

    11-schools_by_topic.py

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -7,5 +7,5 @@ def schools_by_topic(mongo_collection, topic):
     mongo_collection will be the pymongo collection object
     topic (string) will be topic searched
     """
-    schlist = mongo_collection.find({"topics": topic}))
-    return list(schlist)
+    schlist = mongo_collection.find({"topics": topic})
+    return (list(schlist))

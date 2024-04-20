4a62a9c4a218409febbf9c2249941f5400a4576f
Mtiass <MDLSproff@gmail.com>
2024-04-20 13:12:36 -0300

    Task 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,1 +1,83 @@
 #!/usr/bin/env python3
+"""
+Simple pagination
+"""
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Method get_page
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_in, end_in = self.index_range(page, page_size)
+
+        dataset = self.dataset()
+        if start_in < len(dataset) and end_in <= len(dataset):
+            return dataset[start_in:end_in]
+        return []
+
+    def index_range(self, page, page_size):
+        """
+        Function that takes two integer arg.
+        """
+        start_in = (page - 1) * page_size
+        end_in = page * page_size
+        return start_in, end_in
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """
+        Method that returns a dictionary containing the following
+        key-value pairs:
+            page_size: the length of the returned dataset page
+            page: the current page number
+            data: the dataset page (equivalent to return from previous task)
+            next_page: number of the next page, None if no next page
+            prev_page: number of the previous page, None if no previous page
+            total_pages: the total number of pages in the dataset as an
+                integer
+        """
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        data = self.get_page(page, page_size)
+
+        if page < total_pages:
+            next_page = page + 1
+        else:
+            next_page = None
+        if page > 1:
+            prev_page = page - 1
+        else:
+            prev_page = None
+
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }

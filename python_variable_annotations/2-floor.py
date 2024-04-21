8245234e32a0009ae4d5ce153ee8f0721a8b815e
Mtiass <MDLSproff@gmail.com>
2024-04-21 00:03:33 -0300

    Task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Type-annotated func which takes a float and
+    returns the floor version of the float.
+    """
+    return int(n)

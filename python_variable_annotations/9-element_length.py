5d0c10c1fb0da145eaf359359e031ac769f27484
Mtiass <MDLSproff@gmail.com>
2024-04-21 00:36:44 -0300

    T9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,9 +2,10 @@
 """
 Let's duck type an iterable object
 """
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst):
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Annotate the below function’s parameters and return values
     with the appropriate types

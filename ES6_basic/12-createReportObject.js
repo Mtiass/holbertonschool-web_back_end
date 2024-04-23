ac77dbc1f9ade834adb1c15037796f02351981a9
Mtiass <MDLSproff@gmail.com>
2024-04-23 17:04:42 -0300

    change t12

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,15 +1,10 @@
 export default function createReportObject(employeesList) {
     return {
-      allEmployees: { ...employeesList },
-  
-      getNumberOfDepartments() {
-        let con = 0;
-        for (const dep in this.allEmployees) {
-            if (Object.prototype.hasOwnProperty.call(this.allEmployees, dep)) {
-            con += 1;
-          }
-        }
-        return con;
+      allEmployees: {
+        ...employeesList,
+      },
+      getNumberOfDepartments(employeesList) {
+        return Object.keys(employeesList).length;
       },
     };
-  }
+}

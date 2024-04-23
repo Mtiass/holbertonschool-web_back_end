328498d9dbb21b96a16e48d73cb112cf1bcd04a7
Mtiass <MDLSproff@gmail.com>
2024-04-22 21:40:16 -0300

    syntax fix

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const new_arr = [];
-    for (let idx of array) {
-      new_arr.push(appendString + value);
-    }  
-    return new_arr;
+  const new_arr = [];
+  for (let idx of array) {
+    new_arr.push(appendString + value);
+  }  
+  return new_arr;
 }

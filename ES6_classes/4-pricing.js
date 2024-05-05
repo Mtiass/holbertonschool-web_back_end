9d4d38f6fed29200d4c7fb807193b10c5bb15dfd
Mtiass <MDLSproff@gmail.com>
2024-05-05 18:06:14 -0300

    T4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+// Implement a class named Pricing
+import Currency from './3-currency';
+
+export default class Pricing {
+    constructor(amount, currency) {
+        if (typeof amount !== 'number') {
+            throw new TypeError('amount must be a number')
+        }
+        if (!(currency instanceof Currency)) {
+            throw new TypeError('currency must be an instance of Currency');
+          }
+          this._amount = amount;
+          this._currency = currency;
+        }
+    
+    get amount() {
+        return this._amount;
+    }
+
+    set amount(namount) {
+        if (typeof amount !== 'number') {
+            throw new TypeError('amount must be a number')
+        }
+        this._amount = namount
+    }
+
+    get currency() {
+        return this._currency
+    }
+
+    set currency(ncurrency) {
+        if (!(currency instanceof Currency)) {
+            throw new TypeError('currency must be an instance of Currency');
+        }
+        this._currency = ncurrency;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    }
+    
+    static convertPrice(amount, conversionrate) {
+        return amount * conversionrate;
+    }
+}

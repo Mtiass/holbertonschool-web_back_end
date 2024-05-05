eaca388eab56b5c5b18c3f1edb6ceab658955a1e
Mtiass <MDLSproff@gmail.com>
2024-05-05 18:34:46 -0300

    Task 7

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,40 @@
+// Implement a class named Airport
+
+export default class Airport {
+    constructor(name, code) {
+        if (typeof name !== 'string') {
+            throw new TypeError('name must be a string')
+        }
+        if (typeof code !== 'string') {
+            throw new TypeError('code must be a string')
+        }
+        this._name = name
+        this._code = code
+    }
+
+    get name() {
+        return this._name
+    }
+
+    get code() {
+        return this._code
+    }
+
+    set name(nname) {
+        if (typeof nname !== 'string') {
+            throw new TypeError('name must be a string')
+        }
+        this._name = nname;
+    }
+
+    set code(ncode) {
+        if (typeof ncode !== 'string') {
+            throw new TypeError('code must be a string')
+        }
+        this._code = ncode;
+    }
+
+    toString() {
+        return `[object ${this._code}]`;
+    }
+}

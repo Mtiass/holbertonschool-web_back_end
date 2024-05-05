bef21acd278ba65ed32dbba284a358ba77190bee
Mtiass <MDLSproff@gmail.com>
2024-05-05 13:43:52 -0700

    task 3 fix

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,39 +1,39 @@
 // Implement a class named Currency
 export default class Currency {
-    constructor(code, name) {
-        if (typeof code!== 'string') {
-            throw new TypeError('Code must be a string');
-        }
-        if (typeof name!== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        this._code = code;
-        this._name = name;
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
     }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
 
-    get code() {
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set code(ncode) {
-        if (typeof ncode!== 'string') {
-            throw new TypeError('Code must be a string');
-        }
-        this._code = ncode;
+  set code(ncode) {
+    if (typeof ncode !== 'string') {
+      throw new TypeError('Code must be a string');
     }
+    this._code = ncode;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(nname) {
-        if (typeof nname!== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        this._name = nname;
+  set name(nname) {
+    if (typeof nname !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = nname;
+  }
 
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-    }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }

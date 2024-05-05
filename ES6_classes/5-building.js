17d9c03d549694cf999b04bd058f47881038c2c1
Mtiass <MDLSproff@gmail.com>
2024-05-05 14:18:03 -0700

    5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,25 +1,25 @@
 // Implement a class named Building
 
 export default class Building {
-    constructor(sqft) {
-        if (typeof sqft !== 'number') {
-            throw new TypeError('sqft must be a number')
-        }
-        this._sqft = sqft;
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    set sqft(nsqft) {
-        if (typeof nsqft !== 'number') {
-            throw new TypeError('sqft must be a number')
-        }
-        this._sqft = nsqftsqft;
+  set sqft(nsqft) {
+    if (typeof nsqft !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
+    this._sqft = nsqft;
+  }
 
-    evacuationWarningMessage() {
-        throw new TypeError('Classes extending the "Building" abstract class must implement "evacuationWarningMessage" method');
-    }
+  evacuationWarningMessage() {
+    throw new TypeError("Class extending Building must override evacuationWarningMessage");
+  }
 }

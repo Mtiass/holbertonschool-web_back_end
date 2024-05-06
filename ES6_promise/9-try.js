2cde7a26ffb998720d0112c5515eba5c19bb8887
Mtiass <MDLSproff@gmail.com>
2024-05-06 07:18:43 -0700

    T9

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,11 +1,11 @@
 // Write a function named guardrail that will accept one argument mathFunction.
 export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-        queue.push(mathFunction(), 'Guardrail was processed');
-    } catch (error) {
-        queue.push(error.toString(), 'Guardrail was processed')
-    }
+  const queue = [];
+  try {
+    queue.push(mathFunction(), 'Guardrail was processed');
+  } catch (error) {
+    queue.push(error.toString(), 'Guardrail was processed');
+  }
 
-    return queue;
+  return queue;
 }

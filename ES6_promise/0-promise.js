43724043cba4b708af634648a85424ee132f7d90
Mtiass <MDLSproff@gmail.com>
2024-05-06 10:03:33 -0300

    T0

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+// Return a Promise using this prototype function getResponseFromAPI()
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve();
+        }
+        else {
+            reject();
+        }
+    });
+}

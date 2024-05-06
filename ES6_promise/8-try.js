27a93e4dbfad8fbf3698d6e7630225ff8b325494
Mtiass <MDLSproff@gmail.com>
2024-05-06 07:12:15 -0700

    T8

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,8 +1,8 @@
-// Function named divideFunction that will accept two arguments: numerator (Number) and denominator (Number).
+// Function named divideFunction that will accept two arguments: numerator and denominator.
 export default function divideFunction(numerator, denominator) {
-    if (denominator === 0) {
-        throw Error('cannot divide by 0');
-    } else {
-        return (numerator / denominator);
-    }
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return (numerator / denominator);
+  }
 }

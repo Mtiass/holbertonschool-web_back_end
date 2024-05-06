ad4698dca16c8908cfedb0a7c36c9966e062aa2f
Mtiass <MDLSproff@gmail.com>
2024-05-06 06:28:44 -0700

    T2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 // Catch me if you can!
 export default function handleResponseFromAPI(promise) {
-    return promise
+  return promise
     .then(() => ({
-            status: 200,
-            body: 'success',
-        }))
+      status: 200,
+      body: 'success',
+    }))
     .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
 }

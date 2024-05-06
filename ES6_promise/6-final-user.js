9ad0b5df5b0be4034f8a29cbacd808ac0a2532a2
Mtiass <MDLSproff@gmail.com>
2024-05-06 07:00:06 -0700

    lcdmhdm

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,6 @@
 //  The function should call the two other functions.
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);

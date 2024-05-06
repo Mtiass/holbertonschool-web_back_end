0d0f80473ab8fdcf03b9dee8b78ece523dd83f61
Mtiass <MDLSproff@gmail.com>
2024-05-06 06:48:00 -0700

    5th task

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
 // The function should return a Promise rejecting with an Error.
 export default function uploadPhoto(fileName) {
-    return Promise.reject(
-        new Error(`${fileName} cannot be processed`),
-    );
+  return Promise.reject(
+    new Error(`${fileName} cannot be processed`),
+  );
 }

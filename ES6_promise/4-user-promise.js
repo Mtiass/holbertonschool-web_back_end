d296d116845cd55d5ef35a1477516175f2113161
Mtiass <MDLSproff@gmail.com>
2024-05-06 06:43:48 -0700

    Task 4

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,7 +1,7 @@
 // That returns a resolved promise with this object:
 export default function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-            firstName,
-            lastName,
-        });
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
 }

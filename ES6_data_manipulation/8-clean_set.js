c6b555822f6baa2b66a989d6107a5db277160765
Mtiass <MDLSproff@gmail.com>
2024-05-07 12:19:38 -0700

    Task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 // Function cleanSet, returns a string of all the set values that start with string (startString).
 export default function cleanSet(set, startString) {
   const array = Array.from(set);
-  const filterarray = array.filter(value => value.startsWith(startString));
-  const extracarray = filterarray.map(value => value.substring(startString.length));
+  const filterarray = array.filter((value) => value.startsWith(startString));
+  const extracarray = filterarray.map((value) => value.substring(startString.length));
   const result = extracarray.join('-');
   return result;
 }

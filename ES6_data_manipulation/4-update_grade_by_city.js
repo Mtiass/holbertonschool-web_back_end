178164303eea7758e99db058f480becda805b5ce
Mtiass <MDLSproff@gmail.com>
2024-05-07 15:22:12 -0300

    task 4 is too difficult

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,3 @@
+// Function updateStudentGradeByCity that returns an array of students for a specific city with their new grade
+export default function updateStudentGradeByCity(students, city, newGrades) {
+}

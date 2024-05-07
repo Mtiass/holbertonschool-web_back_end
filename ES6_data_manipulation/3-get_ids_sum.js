28831211e94fecc37165a0db7b31c03a52d7ad56
Mtiass <MDLSproff@gmail.com>
2024-05-07 11:15:57 -0700

    Task 3

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 // Function getStudentIdsSum that returns the sum of all the student ids.
 export default function getStudentIdsSum(students) {
-    return (students.reduce((add, student) => add + student.id, 0));
+  return (students.reduce((add, student) => add + student.id, 0));
 }

bc11b5a5f788fa3ec742502f0cf24f0f3b0d0d06
Mtiass <MDLSproff@gmail.com>
2024-05-07 12:26:09 -0700

    9-groceries_list.js

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
 // Function named groceriesList that returns a map of groceries with the following items.
 export default function () {
-    const groceries = new Map([
-        ['Apples', 10],
-        ['Tomatoes', 10],
-        ['Pasta', 1],
-        ['Rice', 1],
-        ['Banana', 5],
-    ]);
-    return groceries;
+  const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return groceries;
 }

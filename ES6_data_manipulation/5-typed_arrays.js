cb520e808a6e38180c55ec112aaab97d2de2be1e
Mtiass <MDLSproff@gmail.com>
2024-05-07 11:30:13 -0700

    Task 5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 // Function createInt8TypedArray, returns an ArrayBuffer with an Int8 value at a spec position.
 export default function createInt8TypedArray(length, position, value) {
-    if (position < 0 || position >= length) {
-        throw new Error("Position outside range");
-    }
-    const buff = new ArrayBuffer(length);
-    const int8Array = new Int8Array(buff);
-    int8Array[position] = value;
-    return int8Array;
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buff = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buff);
+  int8Array[position] = value;
+  return int8Array;
 }

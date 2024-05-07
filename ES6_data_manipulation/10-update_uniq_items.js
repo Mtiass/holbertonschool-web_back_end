aebb7c59ef608e6c2daf5a0a7da102993ed9a602
Mtiass <MDLSproff@gmail.com>
2024-05-07 12:37:25 -0700

    Task 10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,12 @@
 // Function updateUniqueItems, returns an updated map for all items with init quantity: 1.
 export default function updateUniqueItems(itemsMap) {
-    if (!(itemsMap instanceof Map)) {
-        throw new Error("Cannot process");
+  if (!(itemsMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  itemsMap.forEach((quantity, itemName) => {
+    if (quantity === 1) {
+      itemsMap.set(itemName, 100);
     }
-    itemsMap.forEach((quantity, itemName) => {
-        if (quantity === 1) {
-            itemsMap.set(itemName, 100);
-        }
-    });
-    return itemsMap;
+  });
+  return itemsMap;
 }

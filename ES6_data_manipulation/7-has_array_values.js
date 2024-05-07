3599f1d930f14115ff172b50b4d693bb9b11b0ed
Mtiass <MDLSproff@gmail.com>
2024-05-07 12:10:42 -0700

    7-has_array_values.js

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,4 @@
 // Function hasValuesFromArray, returns a boolean if all the array elements exist within the set.
 export default function hasValuesFromArray(set, array) {
-    return (array.every(val => set.has(value)));
+  return (array.every((val) => set.has(val)));
 }

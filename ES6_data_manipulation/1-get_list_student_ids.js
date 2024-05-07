200c30e57b7ff3db09c98b75f2c425a2a9e7c250
Mtiass <MDLSproff@gmail.com>
2024-05-07 10:59:27 -0700

    task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,7 @@
 // Function getListStudentIds that returns an array of ids from a list of object.
 export default function getListStudentIds(objarray) {
-    if (!Array.isArray(objarray)) {
-        return [];
-    } else {
-        return (objarray.map((student) => student.id));
-    }
+  if (!Array.isArray(objarray)) {
+    return [];
+  }
+  return (objarray.map((student) => student.id));
 }
